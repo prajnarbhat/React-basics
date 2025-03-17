b86f3f3258a40c2f091488c10d503c829466e625
prajnarbhat <rbhatprajna@gmail.com>
2025-03-17 15:41:30 +0530

    Modules Export import statements

---
+++ React-Es6/ModulesExportImport.js/exportingvariables.js
@@ -0,0 +1,4 @@
+export const name = "abc";
+export const age = 20;
+
+// export {name,age}
---
+++ React-Es6/ModulesExportImport.js/message.js
@@ -0,0 +1,8 @@
+const message = () => {
+    let name =  "abc";
+    let city = "bangalore";
+    return "My name" + name +" and city is" + city ; 
+};
+
+// There should be only one default export
+export default message;
