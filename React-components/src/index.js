be29c485a822abfdc343ac262010536d96c41e82
prajnarbhat <rbhatprajna@gmail.com>
2025-03-19 15:59:27 +0530

    React Components

---
+++ React-components/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// importing react in our application
+// ReactDom package
+import ReactDOM from 'react-dom/client';
+//import index.css
+import "./index.css";
+
+// Functional components returns html
+function Car() {
+  return <h1> Car component </h1>
+}
+
+// Sending props to components
+// Props are the functional arguments passed to components as arguments
+function Bike(props) {
+  console.log("Props here is:", props);
+  return (
+    <>
+      <h1> I have a bike which has a colour {props.color} </h1>
+      // Passing components inside another component
+      <Car />
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Bike color="red" />);
+
