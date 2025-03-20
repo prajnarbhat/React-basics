463cfe3df2ff271d95290f3f4a3e4272b3e1cfdb
prajnarbhat <rbhatprajna@gmail.com>
2025-03-20 11:51:25 +0530

    React UseState With Arrays

--- React-useState2/src/App.js
+++ React-useState2/src/App.js
@@ -1,44 +1,46 @@
 import React, { useState } from "react";
-
-
-// 1.Use state which is used to change the state of application.
-// 2.First need to be imported.
-// 3.UseState hook is a function, useState().
-// 4.It returns an array with 2 values.
-// 5.console.log(useState()) => returns [ undefined,function ].
-// 6.The function present inside array will edit,change or manipulate the text/string/Ds present inside.
-// 7.useState("Hello")  => returns [ "hello",f ]  here it can be boolean,string or any DS
-// 8. function is used to manipulate the value present in array
+// First import data
+import { data } from './data.js';
 
 function App() {
-    // console.log("UseState:", useState(1));   // [1, ƒ]
-    // console.log("First values inside the useState array:", useState(1)[0]);
-    // // First values inside the useState array: 1
-    // console.log("Second values inside the useState array:", useState(1)[1]);
-    // // function" === typeof JSCompiler_OptimizeArgumentsArray_p1 && console.error("State updates from the useState() and u…
-    const [ text, setText] = useState("Micro");
-    function handleClick() {
-        console.log("Text value:", text);
-        // Now use setText to manipulate text after button is clicked
-        if( text == "Micro") {
-            setText("Mini");
-        }else {
-            setText("Micro");
-        }    
+    // 1. Use useState to get the array data
+    // 2. Now team will have the data stored
+    const [teams, setTeam] = useState(data);
+    console.log("team:", teams);
+    /* 0: {id: 1, name: 'Gaurav'} 1: {id: 2, name: 'abc'} 2: {id: 3, name: 'xyz'}length: 3[[Prototype]]: Array(0)*/
+    // 3. Returns an array of object
+    // 4. Using map we need to access the array of objects to get individual property or value from the array
+    function clearTeam() {
+        setTeam([])
     }
     return (
         <>
-            <h1> UseState </h1>
-            <h3> {text} </h3>
-            <button type="button" onClick={handleClick}> Click me</button>
-        </>        
+            <h1> Accessing array of object using useState </h1>
+            {teams.map((team) => {
+                console.log("Get each item of array:", team); //{id: 1, name: 'Gaurav'}
+                return (
+                    <div key={team.id}>
+                        <h4> {team.name} </h4>
+                    </div>
+                )
+            })}
+            <button type="button" onClick={clearTeam}> ClearAll </button>
+            
+        </>
     )
+   
+   
 }
 export default App;
 
-// 9.UseState will return an array
-// 10.Array can be destructured
-// 11.const [] = useState("Micro"); array destructuring 
-// 12.const [text] =useState("Micro");
-// 13.here text will be assigned as first values of the array
-// 14.const [text,setText] setText will be assigned with a function which is used to manipulate the text
+// 4.map is used to access individual item of the array and return the name of all the objects in an array
+// 5. return ( <div> {team.name} </div> )
+// 6.We have console error
+//      -> Each child in a list should have a unique "key" prop.
+// 7.teams is a list here each child means each team in teams should have unique prop
+// 8.Here unique property is id as id:1,2,3
+// 9.Pass this unique id as a key to each child 
+//       return (<div key={team.id}>
+// 10.Now create a button if we click the button all the names should be cleared
+// 11.So for that we use setTeam which is used to change the value of team
+// 12.Create an empty array inside setTeam which will clear all items
---
+++ React-useState2/src/data.js
@@ -0,0 +1,16 @@
+// Array of objects
+
+export const data = [
+    {
+        id: 1,
+        name: "Gaurav"
+    },
+    {
+        id: 2,
+        name: "abc"
+    },
+    {
+        id: 3,
+        name: "xyz"
+    }
+]
