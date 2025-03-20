73cc6d03f4d123b2232aef7814d0e19585c04324
prajnarbhat <rbhatprajna@gmail.com>
2025-03-20 11:32:49 +0530

    React useState

--- React-useState/src/index.js
+++ React-useState/src/index.js
@@ -3,20 +3,9 @@ import React from 'react';
 // ReactDom package
 import ReactDOM from 'react-dom/client';
 //import index.css
+import App from "./App.js";
 import "./index.css";
 
-function EventList() {
-  function clickHandler() {
-    alert("Hello world");
-  }
-  return (
-    <>
-      <h1> Basics about Events </h1>
-      <button type="button" onClick={clickHandler}> Click me </button>
-    </>
-  )
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<EventList />);
+root.render(<App />);
 
